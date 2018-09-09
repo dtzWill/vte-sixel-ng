GNOME/vte

lib: Rename VteTerminalPrivate

src/vteinternal.hh
@@ -22,6 +22,7 @@
 
 #include "vtedefines.hh"
 #include "vtetypes.hh"
+#include "vtedraw.hh"
 #include "reaper.hh"
 #include "ring.hh"
 #include "buffer.h"
@@ -323,11 +324,13 @@ private:
         Request *m_request;
 };
 
-/* Terminal private data. */
-class VteTerminalPrivate {
+namespace vte {
+namespace terminal {
+
+class Terminal {
 public:
-        VteTerminalPrivate(VteTerminal *t);
-        ~VteTerminalPrivate();
+        Terminal(VteTerminal *t);
+        ~Terminal();
 
 public:
         VteTerminal *m_terminal;
@@ -434,7 +437,7 @@ public:
         GString *m_selection[LAST_VTE_SELECTION];
         GtkClipboard *m_clipboard[LAST_VTE_SELECTION];
 
-        ClipboardTextRequestGtk<VteTerminalPrivate> m_paste_request;
+        ClipboardTextRequestGtk<Terminal> m_paste_request;
 
 	/* Miscellaneous options. */
         VteEraseBinding m_backspace_binding;
@@ -1354,9 +1357,12 @@ public:
 #undef _VTE_NOP
 };
 
+} // namespace terminal
+} // namespace vte
+
 extern GTimer *process_timer;
 
-VteTerminalPrivate *_vte_terminal_get_impl(VteTerminal *terminal);
+vte::terminal::Terminal* _vte_terminal_get_impl(VteTerminal *terminal);
 
 static inline bool
 _vte_double_equal(double a,

dtzWill/vte-sixel-ng

(cherry picked from commit 8e0c94b22e17f46053ac9d9332514a23991ab44a)

src/vteinternal.hh
@@ -22,6 +22,7 @@
 
 #include "vtedefines.hh"
 #include "vtetypes.hh"
+#include "reaper.hh"
 #include "ring.hh"
 #include "vteconv.h"
 #include "buffer.h"
@@ -326,11 +327,13 @@ private:
         Request *m_request;
 };
 
-/* Terminal private data. */
-class VteTerminalPrivate {
+namespace vte {
+namespace terminal {
+
+class Terminal {
 public:
-        VteTerminalPrivate(VteTerminal *t);
-        ~VteTerminalPrivate();
+        Terminal(VteTerminal *t);
+        ~Terminal();
 
 public:
         VteTerminal *m_terminal;
@@ -428,7 +431,7 @@ public:
         GString *m_selection[LAST_VTE_SELECTION];
         GtkClipboard *m_clipboard[LAST_VTE_SELECTION];
 
-        ClipboardTextRequestGtk<VteTerminalPrivate> m_paste_request;
+        ClipboardTextRequestGtk<Terminal> m_paste_request;
 
 	/* Miscellaneous options. */
         VteEraseBinding m_backspace_binding;
@@ -1349,9 +1352,12 @@ public:
 #undef _VTE_NOP
 };
 
+} // namespace terminal
+} // namespace vte
+
 extern GTimer *process_timer;
 
-VteTerminalPrivate *_vte_terminal_get_impl(VteTerminal *terminal);
+vte::terminal::Terminal* _vte_terminal_get_impl(VteTerminal *terminal);
 
 static inline bool
 _vte_double_equal(double a,